56a7634f0dcfccabe8938d7fb3c5c1b54316f075
dylancraven <dylancraven98403@gmail.com>
2017-12-16 17:33:30 +0100

    Finished Scen_2 Natives, small updates to Scen_1 Natives (add betapie)

--- Scripts/1_Scen1_Natives_calc.R
+++ Scripts/1_Scen1_Natives_calc.R
@@ -8,7 +8,8 @@ require(vegan)
 require(ggplot2)
 require(ggsci)
 require(iNEXT)
-#require(mobr)
+require(BBmisc)
+require(mobr)
 
 ######################
 # JUST NATIVE ########
@@ -126,4 +127,73 @@ radd$geo_entity2<-as.factor(radd$geo_entity2)
 radd$geo_entity2<-factor(radd$geo_entity2,levels=c("Hawai'i Island","Maui Nui","O'ahu Island","Kaua'i Island"))
 
 write.table(radd,"/homes/dc78cahe/Dropbox (iDiv)/Research_projects/Veg. monitoring databases/databases and field protocols/database/IslandForests/Hawaii_only/Diversity_Age/Hawaii_diversity/Cleaned_Data/Scen1_Natives_RAD.csv",sep=",",row.names=F)
+
+
+############
+# BetaPIE  #
+############
+
+# community matrix
+
+h_comm3<-dcast(datt, PlotIDn~SPP_CODE3A, value.var="Abundance_ha",sum)
+rownames(h_comm3)<-h_comm3$PlotIDn
+h_comm3<-select(h_comm3,-PlotIDn)
+
+# group information
+
+h_attr<-unique(select(datt,PlotIDn, geo_entity2))
+
+h_attr<-arrange(h_attr,PlotIDn)
+
+h_attr<-data.frame(h_attr)
+rownames(h_attr)<-h_attr$PlotIDn
+h_attr<-select(h_attr,-PlotIDn)
+colnames(h_attr)<-"group"
+h_attr$group<-as.factor(h_attr$group)
+
+# make mob structure
+h_mob_in <- make_mob_in(h_comm3, h_attr)
+
+h_stats <- get_mob_stats(h_mob_in, group_var = "group",nperm=10)
+
+h_betapie<-data.frame(h_stats$samples$beta_ENS_PIE, h_stats$samples$beta_S)
+h_betapie$PlotIDn<-rownames(h_betapie)
+colnames(h_betapie)[1]<-"beta_ENS_PIE"
+colnames(h_betapie)[2]<-"beta_S"
+
+h_help<-unique(select(datt, geo_entity2, PlotIDn))
+
+h_betapie<-merge(h_help,h_betapie,by.y="PlotIDn")
+
+h_betapie<-select(h_betapie, geo_entity2, PlotIDn, beta_S, beta_ENS_PIE)
+
+
+write.table(h_betapie,"/homes/dc78cahe/Dropbox (iDiv)/Research_projects/Veg. monitoring databases/databases and field protocols/database/IslandForests/Hawaii_only/Diversity_Age/Hawaii_diversity/Cleaned_Data/Scen1_Natives_BetaPIE.csv",sep=",",row.names=F)
+
+
+#####################
+# Environmental data#
+#####################
+
+
+datt_unique<-distinct(select(datt, geo_entity2, PlotIDn, MAT, MAP, PET, Elev_m, Plot_Area))
+datt_unique$dupes<-duplicated(datt_unique$PlotIDn)
+datt_unique<-filter(datt_unique, dupes==FALSE)
+datt_unique<-select(datt_unique,-dupes)
+
+rangezz<-summarize(group_by(datt_unique,geo_entity2),min_MAT=min(MAT),max_MAT=max(MAT), mean_MAT=mean(MAT),
+                   min_MAP=min(MAP),max_MAP=max(MAP),mean_MAP=mean(MAP),
+                   min_PET=min(PET),max_PET=max(PET),mean_PET=mean(PET),
+                   min_Elev=min(Elev_m),max_Elev=max(Elev_m),
+                   totPlotArea=sum(Plot_Area), PlotN=length(unique(PlotIDn)))
+
+rangezz$r_MAT<-rangezz$max_MAT-rangezz$min_MAT
+rangezz$r_MAP<-rangezz$max_MAP-rangezz$min_MAP
+rangezz$r_PET<-rangezz$max_PET-rangezz$min_PET
+rangezz$r_Elev<-rangezz$max_Elev-rangezz$min_Elev
+
+rangezz<-select(rangezz,geo_entity2, totPlotArea, PlotN, mean_MAT, r_MAT, mean_MAP,r_MAP, mean_PET, r_PET, max_Elev,r_Elev)
+
+
+write.table(rangezz,"/homes/dc78cahe/Dropbox (iDiv)/Research_projects/Veg. monitoring databases/databases and field protocols/database/IslandForests/Hawaii_only/Diversity_Age/Hawaii_diversity/Cleaned_Data/Scen1_Natives_EnvConditions.csv",sep=",",row.names=F)
 

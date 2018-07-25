173a60abd2368aaec9c54eeac6c5e7d9e1298036
dylancraven <dylancraven98403@gmail.com>
2018-07-25 15:40:04 +0200

    Added sample-based rarefied richness as suppl. figure

--- Scripts/3_Figures_macroecol_div_rarefiedSppR.R
+++ Scripts/3_Figures_macroecol_div_rarefiedSppR.R
@@ -9,61 +9,6 @@ require(reshape2)
 # data ##
 #########
 
-macro<-read.csv("Data/Hawaii_Div_Macro_SIE.csv",sep=",",header=T)
-
-macroo<-melt(macro, id.vars="geo_entity2", measure.vars=c("nativeSIE","native_woody_spp","total_SppN"),variable.name="Div",value.name="Div2")
-
-macroo$geo_entity2<-as.factor(macroo$geo_entity2)
-macroo$geo_entity2<-factor(macroo$geo_entity2,levels=c("Hawai'i Island","Maui Nui","O'ahu Island","Kaua'i Island"))
-
-macroo$Div<-as.character(macroo$Div)
-macroo$Div<-ifelse(macroo$Div=="nativeSIE","Single Island Endemic",macroo$Div)
-macroo$Div<-ifelse(macroo$Div=="total_SppN","Total Woody Spp.",macroo$Div)
-macroo$Div<-ifelse(macroo$Div=="native_woody_spp","Native Woody Spp.",macroo$Div)
-
-macroo$Div<-as.factor(macroo$Div)
-macroo$Div<-factor(macroo$Div,levels=c("Single Island Endemic","Native Woody Spp.","Total Woody Spp."))
-
-macroo2<-filter(macroo, Div!="Total Woody Spp.")
-
-###########
-# figure ##
-###########
-
-macro_g<-ggplot(macroo2,aes(x=geo_entity2,y=Div2,colour=Div, fill=Div))+ 
-  geom_bar(stat="identity",position=position_dodge())+
-  scale_x_discrete(labels=c("Hawai'i Island"="Hawai'i","Maui Nui"="Maui Nui","O'ahu Island"="O'ahu","Kaua'i Island"="Kaua'i"))+
-  #scale_color_d3(palette="category20c")+ scale_fill_d3(palette="category20c")+
-  scale_fill_manual(values=c("#00BF9A","#008975"))+
-  scale_colour_manual(values=c("#00BF9A","#008975"))+
-  
-  scale_y_continuous(breaks=c(0,50, 100, 150, 200, 250, 300))+
-  guides(colour=FALSE,fill=guide_legend(title="",title.position = "top"))+
-  labs(x="",y="Species diversity")+
-  theme_bw()+theme(legend.position="top", 
-                   axis.title.y=element_text(colour="black",face="bold",size=8),
-                   axis.title.x=element_text(colour="black",face="bold",size=6),
-                   axis.text.x=element_text(colour=c("black"),face="bold",size=8),
-                   axis.text.y=element_text(colour=c("black"),face="bold",size=8),
-                   legend.text=element_text(colour=c("black"),face="bold",size=7),
-                   legend.title = element_text(colour=c("black"),face="bold",size=6),
-                   legend.title.align = 0.5,
-                   legend.key.size = unit(1,"line"),
-                   legend.margin =margin(t=0, r=0, b=0, l=0, unit="cm"),
-                   panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
-###############
-# get colors ##
-###############
-
-g<-ggplot_build(macro_g)
-
-unique(g$data[[1]]["fill"])
-
-##############################################
-# bar graph with rarefied species richness   #
-##############################################
-
 #########
 # data ##
 #########
@@ -111,10 +56,10 @@ All_bar<-ggplot(SppR,aes(x=Loc_Rare,y=S,colour=Rarefy2, fill=Rarefy2))+
                    axis.title.x=element_text(colour="black",face="bold",size=6),
                    axis.text.x=element_text(colour=c("black"),face="bold",size=8, hjust=c(0.2,0,0,0)),
                    axis.text.y=element_text(colour=c("black"),face="bold",size=8),
-                   legend.text=element_text(colour=c("black"),face="bold",size=7),
-                   legend.title = element_text(colour="black",face="bold",size=7),
+                   legend.text=element_text(colour=c("black"),face="bold",size=6),
+                   legend.title = element_text(colour="black",face="bold",size=6),
                    legend.title.align = 0.5,
-                   legend.key.size = unit(1,"line"),
+                   legend.key.size = unit(2,"line"),
                    legend.margin =margin(t=0, r=0, b=0, l=0, unit="cm"),
                    panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
@@ -122,16 +67,16 @@ All_bar<-ggplot(SppR,aes(x=Loc_Rare,y=S,colour=Rarefy2, fill=Rarefy2))+
 # combine panels#
 #################
 
-require(cowplot)
+#require(cowplot)
 
-png(filename="Figures/Macro_Diversity_SppDiv_Fig1.png", 
+png(filename="Figures/IslandLevel_Rare_SppDiv_FigS1.png", 
     units="in", 
     width=4, 
-    height=8, 
+    height=3.5, 
     pointsize=2, 
     res=400)
 
-plot_grid(macro_g, All_bar,labels=c("a)","b)"),label_size=7,ncol=1)
+All_bar
 
 
 dev.off()
